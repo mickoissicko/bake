a55885f3fa68cd75a1d07acf31d840d4bf77bdef
mickoissicko <defualtusermick@gmail.com>
2024-03-11 16:52:49 +0100

    programmed the main functionms

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,37 @@
+#include "includes/imp.hpp"
+
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstdlib>
 
-namespace defs{
-    typedef std::ifstream isf;
-    typedef std::ofstream osf;
-}
-
-int main()
-{
+int main() {
     defs::isf BakeFile("Cook.bake");
 
+    if (!BakeFile.is_open())
+    {
+        std::cerr << "Error opening Cook.bake" << std::endl;
+        return 1;
+    }
 
+    std::string line;
+    while (std::getline(BakeFile, line))
+    {
+        if (!line.empty())
+        {
+            size_t start = line.find(":");
+            size_t end = line.rfind(":");
+
+            if (start != std::string::npos && end != std::string::npos && start < end)
+            {
+                std::string command = line.substr(start + 1, end - start - 1);
+                Cook(command);
+            }
+        }
+    }
 
     BakeFile.close();
 
     return 0;
 }
+
