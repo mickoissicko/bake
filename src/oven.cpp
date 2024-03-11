a3895f96811590ae9323fb84341e87a81b50250c
mickoissicko <defualtusermick@gmail.com>
2024-03-11 17:22:50 +0100

    oven function

---
+++ src/oven.cpp
@@ -0,0 +1,34 @@
+#include "includes/imp.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+void oven() {
+    defs::isf BakeFile("Bakefile");
+
+    if (!BakeFile.is_open())
+    {
+        std::cerr << "No files?" << std::endl;
+    }
+
+    std::string line;
+    while (std::getline(BakeFile, line))
+    {
+        if (!line.empty())
+        {
+            size_t start = line.find(":");
+            size_t end = line.rfind(":");
+
+            if (start != std::string::npos && end != std::string::npos && start < end)
+            {
+                std::string command = line.substr(start + 1, end - start - 1);
+                Cook(command);
+            }
+        }
+    }
+
+    BakeFile.close();
+}
+
