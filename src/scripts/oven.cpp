f02c68098b6fc141d7769ccdb1755178d0862972
mickoissicko <defualtusermick@gmail.com>
2024-03-11 18:14:52 +0100

    better logic to check if >start and { + } exist     if they do, itll only execute commands and even then commands have to be encased within colons :: i.e. : command :

--- src/scripts/oven.cpp
+++ src/scripts/oven.cpp
@@ -1,34 +1,61 @@
 #include "../includes/imp.hpp"
 
+#include <cstddef>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cstdlib>
+#include <cstdbool>
 
-void oven() {
+// so i dont have to use 'defs::str::npos' because what's the point then?
+using namespace defs;
+
+void oven()
+{
     defs::isf BakeFile("Bakefile");
 
     if (!BakeFile.is_open())
     {
         std::cerr << "No files?" << std::endl;
+        return;
     }
 
     std::string line;
+    
+    bool ShouldExecute = false;
+
     while (std::getline(BakeFile, line))
     {
         if (!line.empty())
         {
-            size_t start = line.find(":");
-            size_t end = line.rfind(":");
-
-            if (start != std::string::npos && end != std::string::npos && start < end)
+            if (
+                (line.find(">cook") != str::npos   || 
+                 line.find(">bake") != str::npos   ||  // ._. rawr
+                 line.find(">serve") != str::npos) && 
+                 line.find("{") != str::npos
+            )
             {
-                std::string command = line.substr(start + 1, end - start - 1);
-                Cook(command);
+                ShouldExecute = true;
+            }
+
+            if (ShouldExecute && line.find("}") == 0)
+            {
+                ShouldExecute = false;
+            }
+
+            if (ShouldExecute)
+            {
+                size_t start = line.find(":");
+                size_t end = line.rfind(":");
+
+                if (start != str::npos && end != str::npos && start < end)
+                {
+                    std::string command = line.substr(start + 1, end - start - 1);
+                    Cook(command);
+                }
             }
         }
     }
 
     BakeFile.close();
 }
-
