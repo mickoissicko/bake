b27556cc93c3ebee2727703b5983882f8ad42840
mickoissicko <defualtusermick@gmail.com>
2024-03-11 16:52:20 +0100

    add separate C++ file for bake && add bake func

---
+++ src/bake.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+void Cook(const std::string & command)
+{
+    int result = std::system(command.c_str());
+
+    if (result == 1)
+    {
+        std::cerr << "Error!" << std::endl;
+    }
+}
