5e8d78a16e1e1ce03312ed9b331c97c9df89e211
mickoissicko <mickeysidd@proton.me>
2024-03-12 11:14:29 +0100

    imports to avoid cluttering

---
+++ src/includes/import.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef IMPORT_HPP
+    #define IMPORT_HPP
+#endif
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <map>
