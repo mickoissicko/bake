751cbcae74aac6a95a718622ffe55ca43c38d08f
mickoissicko <mickeysidd@proton.me>
2024-03-12 08:37:48 +0100

    for yaml

---
+++ src/checks.cpp
@@ -0,0 +1,24 @@
+#include "includes/defs.hpp"
+#include "includes/modules.h"
+
+#include <fstream>
+#include <string>
+#include <iostream>
+
+void RunChecks(int argc, char* argv[])
+{
+    defs::isf BakeFile("Bakefile");
+    
+    if (!(BakeFile.is_open()))
+    {
+        std::cerr << "No files?" << std::endl;
+        exit(1);
+    }
+
+    BakeFile.close();
+
+    VerifyFile();
+    Oven();
+
+    Yaml(argc, argv);
+}
